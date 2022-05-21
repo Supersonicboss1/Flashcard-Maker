b0b07729ec94adfb3987941f528227c775eecd50
Supersonicboss1 <leodalgado@gmail.com>
2022-05-21 23:47:21 +0100

    seperated light/dark toggle into component

---
+++ src/components/DarkToggle.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+import { fade } from "svelte/transition";
+import FaMoon from "svelte-icons/fa/FaMoon.svelte";
+import FaSun from "svelte-icons/fa/FaSun.svelte";
+// on:message current value and change
+export let LightModeEnabled: boolean = false;
+function invertLightMode() {
+		// Light -> Dark and vice versa
+		LightModeEnabled = !LightModeEnabled;
+		localStorage.setItem("LightModeEnabled", LightModeEnabled.toString());
+	}
+
+</script>
+
+<div>
+<button
+title={`${
+        LightModeEnabled
+            ? "Enable dark mode"
+            : "Enable light mode"
+    }`}
+on:click={() => invertLightMode()}
+checked={LightModeEnabled}
+class={`${
+    LightModeEnabled ? "bg-slate-700" : "bg-white"
+}
+relative inline-flex shadow-2xl p-1 shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}>
+{#if !LightModeEnabled}
+    <div
+        in:fade={{duration: 200}}
+        class={`
+pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-transparent shadow-lg ring-0 transition duration-200 ease-in-out`}>
+        <FaSun />
+    </div>
+{:else}
+    <div
+        in:fade={{duration: 200}}
+        class={`pointer-events-none inline-block h-[34px] w-[34px] transform rounded-full bg-transparent shadow-lg ring-0 transition duration-200 ease-in-out invert`}>
+        <FaMoon />
+    </div>
+{/if}
+</button>
+</div>
